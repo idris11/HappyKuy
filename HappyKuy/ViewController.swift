6f553c09617f578c47ca7d61e9ef0fc5a3083be8
Idris <idrisnovandi@gmail.com>
2019-05-16 16:40:06 +0700

    Initial Commit

---
+++ HappyKuy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HappyKuy
+//
+//  Created by Idris on 16/05/19.
+//  Copyright © 2019 Pacak Man. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
